2704811c3445f2bfa70b9527c8da88455bb93c64
Gabriel Araújo <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2022-03-15 20:42:04 -0300

    Add files via upload

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -2,7 +2,7 @@ import { toast } from 'react-toastify';
 import api from './api/api';
 
 const LoginService = props => new Promise((resolve, reject) => {
-    api.post('/user/login', props).then( resp =>{
+    api.post('/login', props).then( resp =>{
         if (resp.data) {
             localStorage.setItem('auth-token', resp.data);
             resolve(resp);
