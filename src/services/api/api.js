ceef608c97abaa23cb74fb1ca5605d67a9740964
EuclidesRamos <euclides.filho@ccc.ufcg.edu.br>
2022-03-29 16:09:48 -0300

    Update script and api url

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import { getToken } from '../auth';
 
 const api = axios.create({
-  // baseURL: 'https://otakustech-api.herokuapp.com', // change here, in the future
-  baseURL: 'http://localhost:5000', // change here, in the future
+  baseURL: 'https://otakustech-api.herokuapp.com',
 });
 
 
